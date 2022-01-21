8f1246412a192fa140682b2534ea7b9603b2b5fd
JaIoI <60590709+JaIoI@users.noreply.github.com>
2022-01-21 14:30:43 +0300

    Update lab_16.4.c

--- lab_16/lab_16.4.c
+++ lab_16/lab_16.4.c
@@ -1,23 +1,22 @@
-#include 
- 
+#include <stdio.h>
 int main(void)
 {
-    int a[10];
-    int n, i;
- 
-    printf("N: ");
-    scanf("%i",&n);
- 
-    for (i=0; i<n; ++i){
-        printf("a[%i] : ",i+1);
-        scanf("%i",&a[i]);
-    }
- 
-    for (i=0; i<n/2; ++i){
-            printf("%i\n",a[i]);
-            printf("%i\n",a[n-i-1]);
-    }
-    if (n%2 != 0 ) printf("%i\n",a[(n/2)]);
- 
-    return 0;
+    int a[10];
+    int n, i;
+
+    printf("N: ");
+    scanf("%i",&n);
+
+    for (i=0; i<n; ++i){
+        printf("a[%i] : ",i+1);
+        scanf("%i",&a[i]);
+    }
+
+    for (i=0; i<n/2; ++i){
+        printf("%i\n",a[i]);
+        printf("%i\n",a[n-i-1]);
+    }
+    if (n%2 != 0 ) printf("%i\n",a[(n/2)]);
+
+    return 0;
 }
